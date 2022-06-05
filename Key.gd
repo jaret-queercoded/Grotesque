25d18fd602d8c7ae088cf3d93d5d3bb81cdb2859
Jaret Screws <97352155+jaret-queercoded@users.noreply.github.com>
2022-06-04 20:44:59 -0400

    Get player on ready so we don't have to do when getting key

--- Key.gd
+++ Key.gd
@@ -5,8 +5,12 @@ export var item_name = "test_key"
 export var interaction_text = "Press E to pickup key"
 export var can_interact = true
 
+var player
+
+func _ready():
+	player = get_tree().get_root().get_node("Main").get_node("Player")
+	
 func interact():
-	var player = get_tree().get_root().get_node("Main").get_node("Player")
 	player.add_item(item_name, GameData.item_data[item_name])
 	player.interacting = false
 	queue_free()
